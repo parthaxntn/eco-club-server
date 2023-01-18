005b6ca8bb92e6ac79f60984377d10d601aa333a
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-01-19 00:36:12 +0530

    ADDED - MEMBER MODELS

---
+++ models/MemberModels.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const membersSchema = new Schema({
+
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: false
+            },
+            url: {
+                type: String,
+                required: false
+            }
+        }
+        
+    ],
+    name :{
+        type : String,
+        required : true
+    },
+    designation : {
+        type : String ,
+        required : true
+    },
+
+    date: {
+         type: Date,
+         default: +new Date() + 30*24*60*60*1000 ,
+         required : false
+    },
+
+},{timestamps : true })
+
+module.exports = mongoose.model('members' , membersSchema)
+
+
+
+
+module.exports = mongoose.model('events' , eventsSchema)
