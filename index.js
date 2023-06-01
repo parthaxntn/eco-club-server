ad161da3337e4062b9f0eeada48b36924a252d9b
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-06-01 11:46:40 +0530

    UPDATE: allow origin syntax

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(fileUpload({useTempFiles:true}))
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || process.env.CONSOLE_URL);
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL, process.env.CONSOLE_URL);
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", true);
