873ab8ee05fdb591974c532eacb720c42429b79b
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-06-01 12:03:38 +0530

    FIXED: CORS

--- index.js
+++ index.js
@@ -11,25 +11,34 @@ const multer = require('multer')()
 app.use(express.json())
 
 //config
-if(process.env.NODE_ENV!=="PRODUCTION"){
+if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({ path: "./config/.env" });
 }
 
 // app.use(bodyParser.json());
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-  });
+});
 
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(fileUpload({useTempFiles:true}))
+app.use(fileUpload({ useTempFiles: true }))
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL, process.env.CONSOLE_URL);
+
+    const allowedOrigins = [process.env.CLIENT_URL, process.env.CONSOLE_URL];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    else{
+        res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    }
+
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", true);
@@ -42,7 +51,7 @@ app.use('/api/infos', router)
 app.use('/api/admin', AdminRoutes)
 
 connectMongo().then(
-    app.listen(process.env.PORT,()=>{
+    app.listen(process.env.PORT, () => {
         console.log(`Server is listening on port ${process.env.PORT}`);
     })
 )
